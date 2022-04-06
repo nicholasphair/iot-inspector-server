1062ea4c9f9840c37ae454beaf3c958881ccf15d
Nick Phair <nickphair@gmail.com>
2022-04-06 12:14:07 -0400

    add partner endpoint

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -1,12 +1,13 @@
 import connexion
 import six
 
-from openapi_server.models.fingerprint import Fingerprint    # noqa: E501
-from openapi_server.models.upload import Upload    # noqa: E501
+from openapi_server.models.fingerprint import Fingerprint  # noqa: E501
+from openapi_server.models.partner import Partner  # noqa: E501
+from openapi_server.models.upload import Upload  # noqa: E501
 from openapi_server import util
 
 
-def generate_user_key():    # noqa: E501
+def generate_user_key():  # noqa: E501
     """Generate a new user key.
 
      # noqa: E501
@@ -17,7 +18,7 @@ def generate_user_key():    # noqa: E501
     return 'do some magic!'
 
 
-def heart_beat():    # noqa: E501
+def heart_beat():  # noqa: E501
     """send a heartbeat to the server to let it know we are connected.
 
      # noqa: E501
@@ -28,7 +29,7 @@ def heart_beat():    # noqa: E501
     return 'do some magic!'
 
 
-def home():    # noqa: E501
+def home():  # noqa: E501
     """displays consent page
 
      # noqa: E501
@@ -39,7 +40,18 @@ def home():    # noqa: E501
     return 'do some magic!'
 
 
-def submit_data(user_key, upload=None):    # noqa: E501
+def partner():  # noqa: E501
+    """Ask the server for a partner to compute with.
+
+     # noqa: E501
+
+
+    :rtype: Partner
+    """
+    return 'do some magic!'
+
+
+def submit_data(user_key, upload=None):  # noqa: E501
     """Upload data to the backend.
 
      # noqa: E501
@@ -52,17 +64,11 @@ def submit_data(user_key, upload=None):    # noqa: E501
     :rtype: str
     """
     if connexion.request.is_json:
-        upload = Upload.from_dict(connexion.request.get_json())    # noqa: E501
-    print(upload)
-    # client_action = {start_fast_arp_discovery, quit} U {}
-    # ui_last_active_ts = int
-    # status = 'status'
-    # inspected_devices = []
-    resp = {'status': 'success', 'inspected_devices': []}
-    return resp
+        upload = Upload.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
 
 
-def submit_fingerprint(user_key, fingerprint=None):    # noqa: E501
+def submit_fingerprint(user_key, fingerprint=None):  # noqa: E501
     """Upload device fingerprints to the backend.
 
      # noqa: E501
@@ -75,12 +81,11 @@ def submit_fingerprint(user_key, fingerprint=None):    # noqa: E501
     :rtype: str
     """
     if connexion.request.is_json:
-        fingerprint = Fingerprint.from_dict(connexion.request.get_json())    # noqa: E501
-    print(fingerprint)
+        fingerprint = Fingerprint.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def submit_utc_offset(user_key, offset_seconds):    # noqa: E501
+def submit_utc_offset(user_key, offset_seconds):  # noqa: E501
     """Send client&#39;s timezone to the server.
 
      # noqa: E501
@@ -92,15 +97,10 @@ def submit_utc_offset(user_key, offset_seconds):    # noqa: E501
 
     :rtype: None
     """
-    try:
-        int(offset_seconds)
-        return 'SUCCESS'
-    except ValueError:
-        pass
-    return 'ERROR: Invalid offset_seconds.'
+    return 'do some magic!'
 
 
-def user_user_key(user_key):    # noqa: E501
+def user_user_key(user_key):  # noqa: E501
     """na
 
      # noqa: E501
--- openapi_server/models/__init__.py
+++ openapi_server/models/__init__.py
@@ -4,4 +4,5 @@
 from __future__ import absolute_import
 # import models into model package
 from openapi_server.models.fingerprint import Fingerprint
+from openapi_server.models.partner import Partner
 from openapi_server.models.upload import Upload
